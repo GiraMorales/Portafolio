17ce3b1e5acf06c0c2387654dfd058f583eccf00
Gira <73441352+GiraMorales@users.noreply.github.com>
2024-08-23 12:10:53 +0200

    Add files via upload

--- Data/Data.js
+++ Data/Data.js
@@ -80,12 +80,12 @@ const data = {
         'Mi primer proyecto, con un buen uso de variables CSS, reutilización de estilos con el uso de clases, uso de Grid o Flex. Siendo una página FULL RESPONSIVE con buena semántica y accesibilidad. Uso de alguna meta etiqueta que mejore el SEO. La web contiene enlaces a los productos reales de una página web real y no mía',
       link: 'https://chimerical-stroopwafel-17a509.netlify.app/',
       preview:
-        'https://res.cloudinary.com/dvoady6dt/image/upload/v1724339909/fotos/iawx0m10jzhugwqob9zp.png'
+        'https://res.cloudinary.com/dvoady6dt/image/upload/v1724406684/fotos/it458nxssc9ufjpemetk.png'
     },
     {
       title: 'E-COMMERCE CON FILTROS',
       description:
-        'En este proyecto he aplicado los conocimientos de JavaScript y DOM para crear una página que responda a cambios aplicados por un usuario y sea totalmente funcional. Para ello, he hecho un "modal" que contenga el formulario de los filtros, para que podamos verlo u ocultarlo a nuestro gusto mediante un click. También, he puesto una serie de productos pintados (utilizando DOM y teniendo una estructura de objetos dentro de un array para un pintado sencillo y dinámico), que vayan cambiando según los filtros aplicados. Con un código limpio, claro y manteniendo las buenas prácticas.',
+        'En este proyecto he aplicado los conocimientos de JavaScript y DOM para crear una página que responda a cambios aplicados por un usuario y sea totalmente funcional. Para ello, he hecho un "modal" que contenga el formulario de los filtros, para que podamos verlo u ocultarlo a nuestro gusto mediante un click.<br/> También, he puesto una serie de productos pintados (utilizando DOM y teniendo una estructura de objetos dentro de un array para un pintado sencillo y dinámico), que vayan cambiando según los filtros aplicados. Con un código limpio, claro y manteniendo las buenas prácticas.',
       link: 'hhttps://github.com/GiraMorales/Proyecto2.git',
       preview:
         'https://images.unsplash.com/photo-1657812159103-1b2a52a7f5e8?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -94,9 +94,21 @@ const data = {
       title: 'PORTFOLIO',
       description:
         'Este proyecto he realizado un portfolio atractivo para que las empresas puedan llegar a conocerme y ver los proyectos que he ido realizando y con código limpio, bien estructurado, y divido en componentes.',
-      link: 'https://github.com/giramorales/proyecto3.git',
+      link: 'https://github.com/GiraMorales/Portafolio.git',
       preview:
         'https://images.unsplash.com/photo-1708447134657-2f3f600ee0df?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title: 'REPLICA PINTERES CON UNA API',
+      description:
+        'En este proyecto es una réplica de pinterest. Esta página permite ver diferentes tipos de imágenes mediante la búsqueda que realice un usuario. Es un proyecto hecho con VITE, creando componentes para repartir y reutilizar mejor mi código. Utilizo la API de unsplash para realizar las peticiones y que nos permita obtener diferentes imágenes según la búsqueda. Esta es la documentación de la API: https://unsplash.com/developers.' +
+        '<br/>Las características: si no se encuentran imágenes con la frase introducida, realizará otra petición con la palabra gatos y la sugerencia de utilizar otra frase o palabra para una búsqueda correcta.' +
+        '<br/>El código está correctamente repartido en componentes.' +
+        '<br/>Se limpiará el input después de cada búsqueda para que no moleste al hacer una nueva búsqueda.' +
+        '<br/>Vuelve al estado inicial de la página haciendo click en el propio logo, y que nos muestre la primera petición que se hizo que en este caso es tormenta.',
+      link: 'https://github.com/GiraMorales/Portafolio.git',
+      preview:
+        'https://res.cloudinary.com/dvoady6dt/image/upload/v1724406397/fotos/knn45plap3vukycgxkrs.png'
     }
   ]
 };
