0cc5989eeebebfd4b36709e529c35b55c5b95db6
Gira <73441352+GiraMorales@users.noreply.github.com>
2024-08-28 17:25:48 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import './style.css';
 import './dataStyle.css';
+<<<<<<< HEAD
+import './mediaStyles.css';
+=======
 import './mediaStyle.css';
+>>>>>>> 5560fdea7136004bf5008cebae1d71ae4cca546c
 import Header from './Componentes/Header/Header.js';
 import Footer from './Componentes/Footer/Footer.js';
 import { Main, addMainListeners } from './Componentes/Main/Main.js';
