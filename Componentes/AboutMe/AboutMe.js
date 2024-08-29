8dfa84883ffbc5dda6c5b72c0a71d28c23feb149
GiraMorales <gira.morales@gamil.com>
2024-08-29 12:36:10 +0200

    nuevos cambios crear funcion skill

--- Componentes/AboutMe/AboutMe.js
+++ Componentes/AboutMe/AboutMe.js
@@ -1,21 +1,25 @@
 import data from '../../Data/Data.js';
 import './AboutMe.css';
 
+const skill = [];
+
 const template = () => {
+  // Función para crear el HTML de una tarjeta de habilidad
+  const createSkillCard = (skill) => {
+    return `
+      <li>
+        <img src=${skill.icono} alt=${skill.nombre} class="icono"/> 
+        <h3>${skill.nombre}</h3>
+      </li>
+    `;
+  };
   return `
     <section class="aboutme" id="aboutme">
       <h2>Sobre mi</h2>
       <img class="avatar" src=${data.avatar} alt=${data.name}/>
       <ul>
-      ${data.skills
-        .map(
-          (skill) => `<li>
-      <img src=${skill.icono} alt=${skill.nombre}  class="icono"/> 
-      <h3>${skill.nombre}</h3>
-      </li>`
-        )
-        .join('')}
-    </ul>
+        ${data.skills.map(createSkillCard).join('')}
+        </ul>
       <p>${data.aboutMe}</p>
       <p>${data.address}</p>
       <a href=${`mailto:${data.email}>@ Para ponerse en contacto conmigo haz click aquí @</a>`}
